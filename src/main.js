7f91cef9ac1be247ad636bd33b5c1e3f7cb8de15
mr-dit <mezentsev000@mail.ru>
2024-10-31 13:11:29 +0300

    feat: исп библ компонентов и стилей

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import './assets/main.css'
+import PrimeVue from 'primevue/config'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import Aura from '@primevue/themes/aura'
 
 import App from './App.vue'
 import router from './router'
@@ -10,5 +12,10 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura
+  }
+})
 
 app.mount('#app')
