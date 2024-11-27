20a748a106ad9573de1c8c538f068172f181a83c
mr-dit <mezentsev000@mail.ru>
2024-11-27 23:44:20 +0300

    feat: подкл api для отчетов

--- src/views/BookReport/BookReport.vue
+++ src/views/BookReport/BookReport.vue
@@ -6,72 +6,56 @@
         <!-- Вкладка Напоминание -->
         <TabPanel header="Напоминание">
           <div class="flex justify-end mb-4">
-            <Button label="Экспорт" icon="pi pi-download" @click="exportToCSV('reminders')" />
+            <Button class="w-[150px]" label="Экспорт" @click="exportToCSV('reminders')" />
           </div>
           <DataTable
             :value="reminders"
             :paginator="true"
-            :rows="10"
             scrollable
-            scrollHeight="400px"
             :sortField="sortField"
             :sortOrder="sortOrder"
             @sort="onSort"
+            tableStyle="min-width: 50rem"
+            :rows="rowsPerPage"
+            scrollHeight="95%"
+            :first="firstRow"
+            @page="onPageChange"
+            class="flex-grow mb-4"
+            :style="{ 'min-height': 0, 'min-width': 'auto' }"
           >
             <Column field="title" header="Название" sortable></Column>
-            <Column field="customer" header="Клиент" sortable></Column>
+            <Column field="customerName" header="Клиент" sortable></Column>
             <Column field="dateOfIssue" header="Дата выдачи" sortable></Column>
-            <Column field="returnUntil" header="Срок сдачи" sortable></Column>
+            <Column field="returnDueDate" header="Срок сдачи" sortable></Column>
+            <template #footer>
+              <div class="flex items-center gap-2 mb-4">
+                Страница
+                <Button icon="pi pi-chevron-left" @click="previousPage" :disabled="currentPage <= 1"
+                  ><</Button
+                >
+                <span>
+                  <InputText
+                    type="number"
+                    :value="currentPage"
+                    @input="validatePagination"
+                    @keydown.enter="goToPage"
+                    class="w-20"
+                /></span>
+                <Button
+                  icon="pi pi-chevron-right"
+                  @click="nextPage"
+                  :disabled="currentPage >= totalPages"
+                  >></Button
+                >
+                из {{ totalPages }}
+              </div>
+            </template>
           </DataTable>
         </TabPanel>
 
         <!-- Вкладка История выдачи книги -->
         <TabPanel header="История выдачи книги">
-          <div class="flex flex-col mb-4">
-            <label for="book-id" class="mb-2">Идентификатор книги</label>
-            <input
-              id="book-id"
-              v-model="bookId"
-              class="p-2 border rounded-lg"
-              placeholder="Введите идентификатор книги"
-              @input="fetchBookHistory"
-            />
-          </div>
-          <!-- Информация о книге -->
-          <div v-if="bookInfo" class="mb-4">
-            <h3 class="text-xl font-semibold">{{ bookInfo.title }}</h3>
-            <p class="text-gray-500">{{ bookInfo.subtitle }}</p>
-          </div>
-          <div v-else class="mb-4 text-red-500">
-            Книга не найдена
-          </div>
-
-          <div class="flex justify-end mb-4">
-            <Button label="Экспорт" icon="pi pi-download" @click="exportToCSV('history')" />
-          </div>
-          <DataTable
-            :value="history"
-            :paginator="true"
-            :rows="10"
-            scrollable
-            scrollHeight="400px"
-            :sortField="sortField"
-            :sortOrder="sortOrder"
-            @sort="onSort"
-          >
-            <Column field="customer" header="Клиент" sortable></Column>
-            <Column field="dateOfIssue" header="Дата выдачи" sortable></Column>
-            <Column field="returnDate" header="Дата сдачи" sortable>
-              <template #body="slotProps">
-                <span v-if="slotProps.data.overdue" class="text-red-500 font-bold">
-                  {{ slotProps.data.returnDate }}
-                </span>
-                <span v-else>
-                  {{ slotProps.data.returnDate }}
-                </span>
-              </template>
-            </Column>
-          </DataTable>
+          <HistoryBook />
         </TabPanel>
       </TabView>
     </div>
@@ -79,104 +63,101 @@
 </template>
 
 <script setup>
-import { ref } from 'vue';
-import { saveAs } from 'file-saver';
-import DataTable from 'primevue/datatable';
-import Column from 'primevue/column';
-import TabView from 'primevue/tabview';
-import TabPanel from 'primevue/tabpanel';
-import Button from 'primevue/button';
+import { ref, computed } from 'vue'
+import { saveAs } from 'file-saver'
+import api from '@/api'
+import HistoryBook from './HistoryBook.vue'
 
-const bookId = ref('');
-const bookInfo = ref(null);
-const history = ref([]);
-const sortField = ref('dateOfIssue');
-const sortOrder = ref(-1);
+const sortField = ref('dateOfIssue')
+const sortOrder = ref(-1)
 
-const reminders = ref([
-  {
-    title: 'Moby Dick or The White Whale',
-    customer: 'Jasper Sweet',
-    dateOfIssue: '24.05.17',
-    returnUntil: '14.06.17',
-  },
-  {
-    title: 'The Da Vinci Code',
-    customer: 'Bianca Mendoza',
-    dateOfIssue: '25.05.17',
-    returnUntil: '15.06.17',
-  },
-]);
+const rowsPerPage = 50
+const currentPage = ref(1)
 
-const fetchBookHistory = () => {
-  const bookDatabase = {
-    OL957468W: {
-      title: 'The Da Vinci Code',
-      subtitle: 'A Mystery Thriller',
-      history: [
-        {
-          customer: 'Grace Bridges',
-          dateOfIssue: '26.05.17',
-          returnDate: '16.06.17',
-          overdue: true,
-        },
-        {
-          customer: 'Bianca Mendoza',
-          dateOfIssue: '25.03.17',
-          returnDate: '09.04.17',
-          overdue: false,
-        },
-        {
-          customer: 'Nash Charles',
-          dateOfIssue: '01.03.17',
-          returnDate: '12.03.17',
-          overdue: false,
-        },
-        {
-          customer: 'Jasper Sweet',
-          dateOfIssue: '06.02.17',
-          returnDate: '20.02.17',
-          overdue: true,
-        },
-      ],
-    },
-  };
+const totalPages = computed(() => Math.ceil(reminders.value.length / rowsPerPage))
 
-  if (bookId.value && bookDatabase[bookId.value]) {
-    const book = bookDatabase[bookId.value];
-    bookInfo.value = { title: book.title, subtitle: book.subtitle };
-    history.value = book.history;
-  } else {
-    bookInfo.value = null;
-    history.value = [];
+// Логика для страницы и пагинации
+const firstRow = computed(() => (currentPage.value - 1) * rowsPerPage)
+
+function onPageChange(event) {
+  currentPage.value = event.page + 1
+}
+
+const previousPage = async () => {
+  if (currentPage.value > 1) {
+    currentPage.value--
+    await fetchReminders()
   }
-};
+}
+
+const nextPage = async () => {
+  if (currentPage.value < totalPages.value) {
+    currentPage.value++
+    await fetchReminders()
+  }
+}
+
+const validatePagination = (e) => {
+  const val = e.target.value
+  if (val > 0 && val <= totalPages.value) {
+    currentPage.value = val
+  }
+}
+
+const goToPage = async (e) => {
+  console.log(e.target.value)
+  const val = e.target.value
+
+  if (val > 0 && val <= totalPages.value) {
+    currentPage.value = val
+    await fetchReminders()
+  }
+}
+// ----------------------------
+
+const reminders = ref([])
 
 // Функция экспорта в CSV
-const exportToCSV = (type) => {
-  const data = type === 'reminders' ? reminders.value : history.value;
-  const csvContent = [
-    ['Название', 'Клиент', 'Дата выдачи', 'Дата сдачи'],
-    ...data.map((row) => [
-      row.title || '',
-      row.customer,
-      row.dateOfIssue,
-      row.returnDate || row.returnUntil,
-    ]),
-  ]
-    .map((e) => e.join(','))
-    .join('\n');
+const exportToCSV = async () => {
+  const res = await api.get(`history-export/reminders`, { responseType: 'blob' })
 
-  const blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8;' });
-  const filename = `${type}_report.csv`;
-  saveAs(blob, filename);
-};
+  const blob = new Blob([res.data])
+
+  const headers = res.headers['content-disposition']
+  const filename = headers.split(';')[1].split('=')[1]
+  // const blob = new Blob([res.data], { type: filename.split('.')[1] })
+  // const filename = `${type}_report.csv`
+  saveAs(blob, filename)
+}
 
 // Сортировка
 const onSort = (event) => {
-  sortField.value = event.sortField;
-  sortOrder.value = event.sortOrder;
-};
+  sortField.value = event.sortField
+  sortOrder.value = event.sortOrder
+}
+
+const fetchReminders = async (
+  pagination = { page: currentPage.value - 1, size: rowsPerPage },
+  params = {}
+) => {
+  let url = 'history/reminders' + `?${new URLSearchParams(pagination).toString()}`
+
+  if (Object.keys(params).length > 0) {
+    url += `&${new URLSearchParams(params).toString()}`
+  }
+
+  try {
+    const res = await api.get(url)
+    console.log(res)
+    const page = res.data.page
+    totalPages.value = page.totalPages
+    reminders.value = res.data.content
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+fetchReminders()
 </script>
 
 <style scoped>
@@ -187,5 +168,9 @@ input {
 
 .h-screen {
   padding: 20px;
+}
+
+:deep(.p-datatable-paginator-bottom) {
+  display: none;
 }
 </style>
