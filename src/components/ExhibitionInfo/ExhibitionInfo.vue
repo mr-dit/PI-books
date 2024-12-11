fcdb7ed31c1c6d5b07246cab9e4ab1ad9baf75d3
evaBro100 <kharchenko.eva@gmail.com>
2024-12-12 00:42:54 +0500

    feat.: доб модал окно в упр выставками

--- src/components/ExhibitionInfo/ExhibitionInfo.vue
+++ src/components/ExhibitionInfo/ExhibitionInfo.vue
@@ -1,70 +1,95 @@
 <template>
-  <Dialog
-    :visible="localVisible"
-    modal
-    :header="header"
-    :style="{ width: '50vw' }"
-    :closable="false"
-    @hide="close"
-  >
-    <div class="p-4">
-      <h3 class="font-bold text-xl mb-2">{{ book.title }}</h3>
-      <p v-if="book.description" class="mt-4">{{ book.description }}</p>
-      <p class="text-gray-500 mt-2">
-        Дата первой публикации: {{ book.firstPublishDate || 'Неизвестно' }}
-      </p>
-      <h4 class="font-bold text-lg mt-6">Авторы</h4>
-      <ul class="list-disc pl-6">
-        <li v-for="author in book.authors" :key="author.id">
-          {{ author.name }}
-        </li>
-      </ul>
-      <h4 class="font-bold text-lg mt-6">Темы</h4>
-      <ul class="list-disc pl-6">
-        <li v-for="subject in book.subjects" :key="subject">
-          {{ subject }}
-        </li>
-      </ul>
-    </div>
-    <template #footer>
-      <Button label="Закрыть" @click="close" class="p-button-secondary" />
-    </template>
-  </Dialog>
-</template>
-
-<script setup>
-import { ref, watch } from 'vue'
-import Button from 'primevue/button'
-
-// Пропсы для передачи данных
-defineProps({
-  visible: Boolean,
-  book: {
-    type: Object,
-    default: () => ({})
+    <Dialog
+      :visible="localVisible"
+      modal
+      :header="header"
+      :style="{ width: '50vw' }"
+      :closable="false"
+      @hide="close"
+    >
+      <div class="p-4">
+        <h3 class="font-bold text-xl mb-2">{{ exhibition.name }}</h3>
+        <p v-if="exhibition.description" class="mt-4">{{ exhibition.description }}</p>
+        <p class="text-gray-500 mt-2">
+          Период действия: {{ exhibition.startDate }} - {{ exhibition.endDate }}
+        </p>
+        <h4 class="font-bold text-lg mt-6">Список книг:</h4>
+        <DataTable
+          :value="exhibition.books"
+          :rows="10"
+          tableStyle="min-width: 50rem"
+          scrollable
+          scrollHeight="30vh"
+          :paginator="true"
+          :rowsPerPageOptions="[10]"
+        >
+          <Column field="title" header="Название книги" sortable></Column>
+          <Column field="firstPublishDate" header="Дата первой публикации" sortable></Column>
+          <Column field="description" header="Описание" sortable></Column>
+          <Column field="authors" header="Авторы" sortable>
+            <template #body="slotProps">
+              <ul>
+                <li v-for="author in slotProps.rowData.authors" :key="author.id">{{ author.name }}</li>
+              </ul>
+            </template>
+          </Column>
+          <Column field="subjects" header="Темы" sortable>
+            <template #body="slotProps">
+              <ul>
+                <li v-for="subject in slotProps.rowData.subjects" :key="subject">{{ subject }}</li>
+              </ul>
+            </template>
+          </Column>
+        </DataTable>
+      </div>
+      <template #footer>
+        <Button label="Закрыть" @click="close" class="p-button-secondary" />
+      </template>
+    </Dialog>
+  </template>
+  
+  <script setup>
+  import { ref, watch, defineProps, defineEmits } from 'vue'
+  import Button from 'primevue/button'
+  import DataTable from 'primevue/datatable'
+  import Column from 'primevue/column'
+  
+  // Пропсы для передачи данных
+  const props = defineProps({
+    visible: {
+      type: Boolean,
+      required: true
+    },
+    exhibition: {
+      type: Object,
+      default: () => ({
+        name: '',
+        startDate: '',
+        endDate: '',
+        description: '',
+        books: []
+      })
+    }
+  })
+  
+  // Событие закрытия
+  const emit = defineEmits(['update:visible'])
+  
+  const localVisible = ref(props.visible)
+  
+  watch(() => props.visible, (newValue) => {
+    localVisible.value = newValue
+  })
+  
+  const header = 'Информация о выставке'
+  
+  const close = () => {
+    localVisible.value = false
+    emit('update:visible', false)
   }
-})
-
-// Событие закрытия
-defineEmits(['update:visible'])
-
-// Локальное состояние видимости
-const localVisible = ref(visible)
-
-watch(visible, (newValue) => {
-  localVisible.value = newValue
-})
-
-// Заголовок окна
-const header = ref('Информация о книге')
-
-// Метод закрытия модального окна
-const close = () => {
-  localVisible.value = false
-  emit('update:visible', false)
-}
-</script>
-
-<style scoped>
-/* Стили для модального окна */
-</style>
+  </script>
+  
+  <style scoped>
+  /* Стили для модального окна */
+  </style>
+  
--- src/views/ExhibitionsControl/ExhibitionsControl.vue
+++ src/views/ExhibitionsControl/ExhibitionsControl.vue
@@ -1,199 +1,210 @@
 <template>
-  <div class="h-screen w-screen flex overflow-auto">
+<div class="h-screen w-screen flex overflow-auto">
     <!-- Панель поиска -->
     <FilterMenu :inputs="inputs" @search="onSearch">
-      <template #footer>
+    <template #footer>
         <div class="flex justify-end flex-col">
-          <Button label="Редактировать" @click="onEditUser" />
+        <Button label="Редактировать" @click="onEditUser" />
         </div>
-      </template>
+    </template>
     </FilterMenu>
     <div class="w-3/4 h-full p-4 flex flex-col">
-      <div class="rounded-lg p-4 shadow-md">
+    <div class="rounded-lg p-4 shadow-md">
         <h3 class="font-bold text-lg mb-4">Список выставок</h3>
         <DataTable
-          :value="data"
-          tableStyle="min-width: 50rem"
-          scrollable
-          scrollHeight="95%"
-          :first="firstRow"
-          @page="onPageChange"
-          selectionMode="single"
-          v-model:selection="selectedRow"
-          @row-select="onSelectBook"
-          class="flex-grow mb-4"
-          :style="{ 'min-height': 0, 'min-width': 'auto' }"
+        :value="data"
+        tableStyle="min-width: 50rem"
+        scrollable
+        scrollHeight="95%"
+        :first="firstRow"
+        @page="onPageChange"
+        selectionMode="single"
+        v-model:selection="selectedRow"
+        @row-select="onSelectExhibition"
+        class="flex-grow mb-4"
+        :style="{ 'min-height': 0, 'min-width': 'auto' }"
         >
-          <Column field="name" header="Название" sortable></Column>
-          <Column field="startDate" header="Дата начала" sortable></Column>
-          <Column field="endDate" header="Дата окончания" sortable></Column>
-          <template #footer>
+        <Column field="name" header="Название" sortable></Column>
+        <Column field="startDate" header="Дата начала" sortable></Column>
+        <Column field="endDate" header="Дата окончания" sortable></Column>
+        <template #footer>
             <div class="flex items-center gap-2 mb-4">
-              Страница
-              <Button icon="pi pi-chevron-left" @click="previousPage" :disabled="currentPage <= 1"
-                >></Button
-              >
-              <span>
+            Страница
+            <Button
+                icon="pi pi-chevron-left"
+                @click="previousPage"
+                :disabled="currentPage <= 1"
+            ></Button>
+            <span>
                 <InputText
-                  type="number"
-                  :disabled="totalPages <= 1"
-                  :value="currentPage"
-                  @input="validatePagination"
-                  @keydown.enter="goToPage"
-                  class="w-20"
-              /></span>
-              <Button
+                type="number"
+                :disabled="totalPages <= 1"
+                :value="currentPage"
+                @input="validatePagination"
+                @keydown.enter="goToPage"
+                class="w-20"
+                />
+            </span>
+            <Button
                 icon="pi pi-chevron-right"
                 @click="nextPage"
                 :disabled="currentPage >= totalPages"
-                >></Button
-              >
-              из {{ totalPages }}
+            ></Button>
+            из {{ totalPages }}
             </div>
-          </template>
+        </template>
         </DataTable>
-      </div>
-      <div class="flex flex-row gap-2 mt-4">
-        <Button class="w-[150px]" :disabled="!selectedRow" @click="viewExhibition">
-          Просмотреть
-        </Button>
-        <Button class="w-[150px]" :disabled="!canReturn" @click="returnBook">
-          Редактировать
-        </Button>
-        <Button class="w-[150px]" :disabled="!canReturn" @click="returnBook"> Удалить </Button>
-      </div>
     </div>
-  </div>
+    <div class="flex flex-row gap-2 mt-4">
+        <Button class="w-[150px]" :disabled="!selectedRow" @click="viewExhibition">
+        Просмотреть
+        </Button>
+        <Button class="w-[150px]" :disabled="!selectedRow" @click="editExhibition">
+        Редактировать
+        </Button>
+        <Button class="w-[150px]" :disabled="!selectedRow" @click="deleteExhibition">
+        Удалить
+        </Button>
+    </div>
+    </div>
+    <!-- Модальное окно для просмотра выставки -->
+    <Dialog
+    v-model:visible="isDialogVisible"
+    modal
+    header="Информация о выставке"
+    :style="{ width: '30rem' }"
+    >
+    <ExhibitionInfo 
+    :exhibition="selectedRow" 
+    v-model:selectedRow="selectedRow" 
+    />
+    <template #footer>
+        <Button
+        label="Закрыть"
+        @click="isDialogVisible = false"
+        class="mt-4 bg-primary-500 hover:bg-primary-600 text-white px-4 py-2 rounded"
+        />
+    </template>
+    </Dialog>
+</div>
 </template>
 
 <script setup>
-import { ref, computed, onMounted } from 'vue'
+import { ref, computed } from 'vue'
 import { inputs } from './data'
 import FilterMenu from '@/components/FilterMenu'
 import api from '@/api'
 import ExhibitionInfo from '@/components/ExhibitionInfo/ExhibitionInfo.vue'
 
-const data = ref([]) // Полный список выставок
-const filteredExhibitions = ref([]) // Отфильтрованные выставки
-const selectedExhibition = ref(null)
-const filters = ref({
-  name: '',
-  startDate: '',
-  endDate: ''
-}) // Фильтры для поиска
-const isBookModalVisible = ref(false) // Видимость модального окна книги
-const books = ref([]) // Книги, относящиеся к выбранной выставке
+const props = defineProps({
+book: {
+    type: Object,
+    default: () => ({
+    title: '',
+    firstPublishDate: '',
+    description: '',
+    authors: '',
+    subjects: () => [],
+    authors: () => [],
+    })
+}
+})
 
-const selectedRow = ref(null)
+const data = ref([]) // Полный список выставок
+const selectedRow = ref(null) // Выбранная выставка
+const isBookModalVisible = ref(false) // Состояние видимости модального окна для просмотра выставки
 const rowsPerPage = 10
 const currentPage = ref(1)
 
 const totalPages = computed(() => Math.ceil(data.value.length / rowsPerPage))
 
-const onSearch = async (data) => {
-  console.log(data)
-  await fetchExhibitions(undefined, data)
+const onSearch = async (filters) => {
+console.log(filters)
+await fetchExhibitions(undefined, filters)
 }
 
 // Логика для страницы и пагинации
 const firstRow = computed(() => (currentPage.value - 1) * rowsPerPage)
 
 const onPageChange = (event) => {
-  currentPage.value = event.page + 1
+currentPage.value = event.page + 1
 }
 
 const previousPage = async () => {
-  if (currentPage.value > 1) {
+if (currentPage.value > 1) {
     currentPage.value--
     await fetchExhibitions()
-  }
+}
 }
 
 const nextPage = async () => {
-  if (currentPage.value < totalPages.value) {
+if (currentPage.value < totalPages.value) {
     currentPage.value++
     await fetchExhibitions()
-  }
+}
 }
 
 const validatePagination = (e) => {
-  console.log(e)
-
-  const val = e.target.value
-
-  if (val > 0 && val <= totalPages.value) {
+const val = e.target.value
+if (val > 0 && val <= totalPages.value) {
     currentPage.value = val
-  }
+}
 }
 
 const goToPage = async (e) => {
-  console.log(e.target.value)
-  const val = e.target.value
-
-  if (val > 0 && val <= totalPages.value) {
+const val = e.target.value
+if (val > 0 && val <= totalPages.value) {
     currentPage.value = val
     await fetchExhibitions()
-  }
+}
 }
 
-// Логика фильтрации
-const applyFilters = () => {
-  filteredExhibitions.value = exhibitions.value.filter((exhibition) => {
-    const nameMatch =
-      !filters.value.name ||
-      exhibition.name.toLowerCase().includes(filters.value.name.toLowerCase())
-    const startDateMatch =
-      !filters.value.startDate ||
-      new Date(exhibition.startDate) >= new Date(filters.value.startDate)
-    const endDateMatch =
-      !filters.value.endDate || new Date(exhibition.endDate) <= new Date(filters.value.endDate)
-    return nameMatch && startDateMatch && endDateMatch
-  })
-}
-
-// Выбор выставки
-const viewExhibition = async () => {
-  if (!selectedRow.value) return
-
-  try {
-    const response = await api.get(`/exhibitions/${selectedRow.value.id}/books`)
-    const books = response.data
-    selectedBook.value = books[0] // Выберите первую книгу для примера
-    isBookModalVisible.value = true
-  } catch (error) {
-    console.error('Ошибка при загрузке книг выставки:', error)
-  }
-}
-
+// Получение выставок
 const fetchExhibitions = async (
-  pagination = { page: currentPage.value - 1, size: rowsPerPage },
-  params = {}
+pagination = { page: currentPage.value - 1, size: rowsPerPage },
+params = {}
 ) => {
-  let url = 'exhibitions' + `?${new URLSearchParams(pagination).toString()}`
+let url = 'exhibitions' + `?${new URLSearchParams(pagination).toString()}`
 
-  if (Object.keys(params).length > 0) {
+if (Object.keys(params).length > 0) {
     url += `&${new URLSearchParams(params).toString()}`
-  }
+}
 
-  try {
+try {
     const res = await api.get(url)
-    const content = res.data.content.map((item) => ({
-      ...item
-    }))
+    const content = res.data.content.map((item) => ({ ...item }))
     data.value = content
-  } catch (e) {
+} catch (e) {
     console.log(e)
-  }
+}
 }
 
 fetchExhibitions()
+
+// Обработчик выбора выставки
+const onSelectExhibition = (event) => {
+selectedRow.value = event.data
+}
+
+// Открытие модального окна
+const viewExhibition = async () => {
+if (!selectedRow.value) return
+
+try {
+    const response = await api.get(`/exhibitions/${selectedRow.value.id}/books`)
+    selectedRow.value.books = response.data // Добавляем книги в выбранную выставку
+    isBookModalVisible.value = true // Открытие модального окна
+} catch (error) {
+    console.error('Ошибка при загрузке книг выставки:', error)
+}
+}
 </script>
 
 <style scoped>
 :deep(.p-datatable-paginator-bottom) {
-  display: none;
+display: none;
 }
 :deep(.p-datatable-table) {
-  min-width: auto !important;
+min-width: auto !important;
 }
 </style>
